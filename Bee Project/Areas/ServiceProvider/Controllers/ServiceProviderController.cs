3c86ac106ec120d43861c91a3798e3bf98227ea7
baselqyqony <baselqyqony@gmail.com>
2018-03-16 01:07:14 +0100

    initialize project

---
+++ Bee Project/Areas/ServiceProvider/Controllers/ServiceProviderController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bee_Project.Areas.ServiceProvider.Controllers
+{
+    public class ServiceProviderController : Controller
+    {
+        // GET: ServiceProvider/ServiceProvider
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+        public ActionResult Service()
+        {
+            return View();
+        }
+
+        public ActionResult ServiceDetails()
+        {
+            return View();
+        }
+
+    }
+}
