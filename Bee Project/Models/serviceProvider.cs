5ed754dc11feb82089131ce2f4c8c1b1538e9b55
mohamedsabha <mouhammad.sabha@hotmail.com>
2018-03-17 21:30:35 +0100

    Addclass

--- Bee Project/Models/serviceProvider.cs
+++ Bee Project/Models/serviceProvider.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Bee_Project.Models
 {
-    public class serviceProvider
+    public class ServiceProvider
     {
-        [Required]
-        dadddada
-        public int ID { get; set; }
-     daddasdaddaddada
+
     }
 }
