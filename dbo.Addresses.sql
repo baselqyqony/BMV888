1cfe3af577cfb70c8b7d3573e5c73ef9931bd057
baselqyqony <baselqyqony@gmail.com>
2018-04-05 09:33:12 +0200

    meging

---
+++ dbo.Addresses.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Addresses] (
+    [ID]          INT            IDENTITY (1, 1) NOT NULL,
+    [CountryID]   INT            NOT NULL,
+    [CityID]      INT            NOT NULL,
+    [Ditricit]    NVARCHAR (MAX) NULL,
+    [Street]      NVARCHAR (MAX) NULL,
+    [Floornumber] INT            NULL,
+    [DoorNumber]  INT            NULL,
+    [Building]    NVARCHAR (MAX) NULL,
+    [PotalCode]   NVARCHAR (MAX) NULL,
+    [longitude]   NVARCHAR (MAX) NULL,
+    [ultitude]    NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_dbo.Addresses] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
