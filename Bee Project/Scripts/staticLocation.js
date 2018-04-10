a6f2c849116dbaa595ef9ea121a186181e6ee78d
baselqyqony <baselqyqony@gmail.com>
2018-04-10 17:57:44 +0200

    update setLocation() script

---
+++ Bee Project/Scripts/staticLocation.js
@@ -0,0 +1,26 @@
+﻿   
+var x, y;
+function setLocation(altitude, longitude) {
+
+ x = document.getElementById("altitude");
+    
+ y = document.getElementById("longitude");
+  
+
+getLocation();
+    }
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.value = "0.0";
+        y.value = "0.0";
+    }
+}
+
+function showPosition(position) {
+    x.value = "" + position.coords.latitude;
+    y.value = "" + position.coords.longitude;
+
+     
+}
